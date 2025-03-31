23ec26fb080a969e7dcaa4013a79941658db16ea
Thiago Ferreira Ribeiro <thiago26ribeiro@gmail.com>
2025-03-31 14:42:37 -0300

    Criação da classe main para testes

---
+++ Listas/ConceitoGeral/ListaEstaticaMain.java
@@ -0,0 +1,14 @@
+package Listas.ConceitoGeral;
+
+public class ListaEstaticaMain {
+    public static void main(String[] args) {
+        Lista lista = new ListaEstatica(5);
+
+        lista.adicionar(1);
+        lista.adicionar("Casa");
+        lista.adicionar(9.37);
+
+        lista.imprimir();
+    }
+    
+}
