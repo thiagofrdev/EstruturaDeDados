c1d61617435d57a732a89c8a51b537397a9543ef
Thiago Ferreira Ribeiro <thiago26ribeiro@gmail.com>
2025-03-31 14:39:36 -0300

    Implementação dos métodos principais

--- Listas/ConceitoGeral/ListaEstatica.java
+++ Listas/ConceitoGeral/ListaEstatica.java
@@ -3,48 +3,82 @@ package Listas.ConceitoGeral;
 public class ListaEstatica implements Lista{
     //Variáveis de instância
     Object[] lista;
+    int qtdElementos;
 
     //Contrutores
     public ListaEstatica(){this(10);}
 
     public ListaEstatica(int tamanho){
         lista = new Object[tamanho];
+        qtdElementos = 0;
     }
 
     //Métotos Principais
     @Override
     public void adicionar(Object elemento){
-
+        if(!estaCheia()){
+            lista[qtdElementos] = elemento;
+            qtdElementos++;
+        } else 
+            System.out.println("A lista está cheia!");
     }
 
-    public void adicionarPosicao(int idice, Object elemento){
-        
+    public void adicionarPosicao(int indice, Object elemento){
+        if(!estaCheia()){
+            if(lista[indice] == null){
+                lista[indice] = elemento;
+                qtdElementos++;
+            } else 
+                System.out.println("Já existe um elemento nessa posição!");
+        } else 
+            System.out.println("A lista está cheia!");        
     }
 
     public Object remover(int indice){
-        return null;
+        Object retorno = null;
+        if(!estaVazia()){
+            if(lista[indice] != null){
+                retorno = lista[indice];
+                while (indice < lista.length-1) {
+                    lista[indice] = lista[indice++];
+                    indice++;
+                }
+                qtdElementos--;
+            } else 
+                System.out.println("Não existe elemento nessa posição!");
+        } else 
+            System.out.println("A lista está vazia!");
+        return retorno;
     }
 
     public Object obter(int indice){
-        return null;
+        Object retorno = null;
+        if(!estaVazia()){
+            if(lista[indice] != null)
+                retorno = lista[indice];
+            else 
+                System.out.println("Não existe elemento nessa posição!");
+        } else 
+            System.out.println("A lista está vazia!");
+        return retorno;
     }
     
     //Métodos Secundários
     @Override
     public boolean estaVazia(){
-        return lista[0] == null;
+        return qtdElementos == 0;
     }
 
     @Override
     public boolean estaCheia(){
-        return lista[lista.length-1] != null;
+        return qtdElementos == lista.length-1;
     }
 
     @Override
     public String imprimir(){
         String retorno = "[ ";
-        for (int i = 0; i < lista.length; i++){
-            if (lista[i+1] != null) 
+        for(int i = 0; i < lista.length; i++){
+            if(lista[i+1] != null) 
                 System.out.print(lista[i] + ", ");
             else
                 System.out.print(lista[i]);
