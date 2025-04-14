fe9c0046e98a44959e7b9d8c6fe2e9c825905b38
Thiago Ferreira Ribeiro <thiago26ribeiro@gmail.com>
2025-04-14 17:44:36 -0300

    Criada classe de implementação da interface

--- Listas/ExerciciosGPT/ListaEstatica/ListaDeCompras/ListaDeCompras.java
+++ Listas/ExerciciosGPT/ListaEstatica/ListaDeCompras/ListaDeCompras.java
@@ -1,5 +1,45 @@
 package Listas.ExerciciosGPT.ListaEstatica.ListaDeCompras;
 
-public class ListaDeCompras {
-    
+public class ListaDeCompras implements Listavel{
+    //Variáveis de Instância
+    Object[] lista;
+    int qtdElementos;
+
+    //Construtor
+    public ListaDeCompras(){this(10);}
+
+    public ListaDeCompras(int tamanho){
+        lista = new Object[tamanho];
+        qtdElementos = 0;
+    }
+
+    //Métodos Principais
+    public boolean inserir(int posicao, Object dado){
+        return false;
+    }
+
+    public Object remover(int posicao){
+        return false;
+    }
+
+    public Object buscar(Object elemeto){
+        return false;
+    }
+
+    public boolean atualizar(int posicao, Object dado){
+        return false;
+    }
+
+    //Métodos Secundários
+    public boolean estaCheia(){
+        return false;
+    }
+
+    public boolean estaVazia(){
+        return false;
+    }
+
+    public String imprimir(){
+        return "false";
+    }
 }
