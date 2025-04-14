7479e24cfb4c06a1b7c54d64e62e87ed6535869c
Thiago Ferreira Ribeiro <thiago26ribeiro@gmail.com>
2025-04-14 19:42:15 -0300

    Código do professor (comentado)

---
+++ AltoritimosDeOrdenacao/Aula14042025/AlgoritimoBubbleSort.java
@@ -0,0 +1,44 @@
+package AltoritimosDeOrdenacao.Aula14042025;
+
+public class AlgoritimoBubbleSort {
+    public static void main(String[] args) {
+        int[] arrayBubble = {4,5,2,9,7,3,6,1,8};
+        int passagens = 0;
+
+        while(passagens < arrayBubble.length){
+            for(int i = 0; i < arrayBubble.length-1; i++){
+                if(arrayBubble[i] > arrayBubble[i+1]){
+                    int aux = arrayBubble[i+1];
+                    arrayBubble[i+1] = arrayBubble[i];
+                    arrayBubble[i] = aux;
+                }
+            }
+            passagens++;
+        }
+        for(int i = 0; i < arrayBubble.length; i++){
+            System.out.print(arrayBubble[i]+" ");
+        }
+    }
+}
+
+/*
+public class Ordenação{
+    int dados[] = new {6,8,3,1,9,0,2,5};
+
+    public void bubbleSort(){
+        for (int j = 0; j < dados.lenght-1; i++){
+            for(int i = 0; i < dados.lenght-1-j; i++){
+                if(dados[i] > dados[i+1]){
+                    troca(i, i+1);
+                }
+            }
+        }
+    }
+
+    private troca(int indice1, int indice2){
+        int temp = dados[indice1]
+        dados[indice1] = dados[indice2];
+        dados[indice2] = temp;
+    }
+}
+*/
