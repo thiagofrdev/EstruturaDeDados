cf5bb694635ef65606974faf4e7b8844d2c7cee2
Thiago Ferreira Ribeiro <thiago26ribeiro@gmail.com>
2025-04-14 17:41:12 -0300

    Criada interface Listavel

--- Listas/ExerciciosGPT/ListaEstatica/ListaDeCompras/Listavel.java
+++ Listas/ExerciciosGPT/ListaEstatica/ListaDeCompras/Listavel.java
@@ -1,5 +1,14 @@
 package Listas.ExerciciosGPT.ListaEstatica.ListaDeCompras;
 
-public class Listavel {
-    
+public interface Listavel {
+    //Métodos Principais
+    boolean inserir(int posicao, Object dado);
+    Object remover(int posicao);
+    Object buscar(Object elemeto);
+    boolean atualizar(int posicao, Object dado);
+
+    //Métodos Secundários
+    boolean estaCheia();
+    boolean estaVazia();
+    String imprimir();
 }
