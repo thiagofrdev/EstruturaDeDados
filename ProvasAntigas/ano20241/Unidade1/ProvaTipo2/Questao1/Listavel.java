7638dde6182cf304d43faa55adf31fc522f79a37
Thiago Ferreira Ribeiro <thiago26ribeiro@gmail.com>
2025-04-28 13:31:51 -0300

    Remoção do import

--- ProvasAntigas/ano20241/Unidade1/ProvaTipo2/Questao1/Listavel.java
+++ ProvasAntigas/ano20241/Unidade1/ProvaTipo2/Questao1/Listavel.java
@@ -1,7 +1,6 @@
 /*
 Defina uma interface em Java com as operações necessárias (nome da operação, parâmetros de entrada e tipo de retorno(se necessário)) e a descrição destas operações, para implementar um organizador de receitas culinárias, utilizando a estrutura de dados do tipo Lista. É necessário a gerência de uma lista de receitas com ingredientes, modos de preparo e categorias como "favoritas" ou "testadas".
 */
-import ProvasAntigas.ano20241.Unidade1.ProvaTipo2.Questao1;
 
 public interface Listavel {
     //Métodos Principais
