ed93251ecdda50303dee9d2d089591e7c4f6e33b
Thiago Ferreira Ribeiro <thiago26ribeiro@gmail.com>
2025-04-28 13:50:09 -0300

    Implementado método inserir()

--- ProvasAntigas/ano20241/Unidade1/ProvaTipo2/Questao1/ControleListaDeReceita.java
+++ ProvasAntigas/ano20241/Unidade1/ProvaTipo2/Questao1/ControleListaDeReceita.java
@@ -14,7 +14,17 @@ public class ControleListaDeReceita implements Listavel{
     }
 
     //Métodos Principais
-    public boolean inserir(ListaDeReceita receita){return false;}
+    public boolean inserir(ListaDeReceita receita){
+        boolean retorno = false;
+        if(!estaVazia()){
+            ponteiroFim++;
+            lista[ponteiroFim] = receita;
+            qtdItens++;
+            retorno = true;
+        } else System.out.println("Lista Cheia!");
+        return retorno;
+    }
+    
     public ListaDeReceita verificar(){return null;}
     public boolean atualizar(int posicao, ListaDeReceita receita){return false;}
     public ListaDeReceita remover(int posicao){return null;}
