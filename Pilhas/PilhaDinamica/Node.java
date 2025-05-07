20afebe9df3ec797870ee4e3b79aad04eaac1849
Thiago Ferreira Ribeiro <thiago26ribeiro@gmail.com>
2025-05-07 17:43:28 -0300

    Classe Node para Pilha Dinâmica

---
+++ Pilhas/PilhaDinamica/Node.java
@@ -0,0 +1,13 @@
+package Pilhas.PilhaDinamica;
+
+public class Node<T> {
+    //Variáveis de Instância
+    T data;         //Dado a ser armazenado pelo nó
+    Node<T> next;   //Próximo nó
+
+    //Construtor
+    Node(T data){
+        this.data = data;
+        this.next = null;   //Só vai receber outro Nó quando for colocado mais um na pilha
+    }
+}
