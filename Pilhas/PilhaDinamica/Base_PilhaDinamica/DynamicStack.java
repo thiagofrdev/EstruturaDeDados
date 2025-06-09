36f794d8a700482e30aa7b85184c4a7d7553cf81
Thiago Ferreira Ribeiro <thiago26ribeiro@gmail.com>
2025-06-09 19:17:07 -0300

    Adicionando método empilhar

--- Pilhas/PilhaDinamica/Base_PilhaDinamica/DynamicStack.java
+++ Pilhas/PilhaDinamica/Base_PilhaDinamica/DynamicStack.java
@@ -5,6 +5,14 @@ public class DynamicStack<T> implements Stack<T> {
     private Node<T> top;    //Ponteiro para o Nó do topo
     private int size;       //Quandidade de nós na pilha
 
+    //Métodos primários
+    public void empilhar(T dado){
+        Node<T> newNode = new Node<>(dado);
+        newNode.next = top;     //Liga o novo nó ao atual topo (que será o anterior ao novo)
+        top = newNode;          //Atualiza o topo
+        size++;
+    }
+
     //Métodos secundários
     public boolean estaVazia(){
         return size == 0;
