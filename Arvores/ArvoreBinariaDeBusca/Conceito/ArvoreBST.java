ff0daa3365cea9393d60f0becf08e5a39ffbc7be
Thiago Ferreira Ribeiro <thiago26ribeiro@gmail.com>
2025-07-14 17:15:32 -0300

    Implementado método de inserir em uma BST

--- Arvores/ArvoreBinariaDeBusca/Conceito/ArvoreBST.java
+++ Arvores/ArvoreBinariaDeBusca/Conceito/ArvoreBST.java
@@ -8,7 +8,34 @@ public class ArvoreBST<T extends Comparable<T>> implements Arborizavel<T> {
     }
 
     public void inserir(T valor){
-        System.out.println("Método inserir");
+        NoTriplo<T> novoNo = new NoTriplo<>();
+        novoNo.setDado(valor);
+
+        //Confere se a árvore esstá vazia. Se estiver, cria o nó raiz
+        if(raiz == null){
+            raiz = novoNo;
+        } else {
+            NoTriplo<T> noAuxiliar = raiz; //Nó que vai passear pelos valores da árvore até achar o lugar onde o novo nó será inserido
+            while (true) {
+                int comparacao = valor.compareTo(noAuxiliar.getDado()); //Compara o valor a ser inserido com o dado que está em noAuxiliar
+                if (comparacao <= 0) { //IR PARA ESQUERDA
+                    if (noAuxiliar.getEsquerda() == null) { //Se a esquerda tiver vazia, adiciona
+                        noAuxiliar.setEsquerda(novoNo);
+                        novoNo.setGenitor(noAuxiliar);
+                        break;
+                    } //Se tiver valor na esquerda...
+                    noAuxiliar = noAuxiliar.getEsquerda(); //...pega o valor da esquerda de noAuxiliar e reatribui a noAuxiliar para uma nova comparação
+                } else {
+                    //IR PARA DIREITA
+                    if (noAuxiliar.getDireita() == null) { //Se a direita tiver vazia, adiciona
+                        noAuxiliar.setDireita(novoNo);
+                        novoNo.setGenitor(noAuxiliar);
+                        break;
+                    } //Se tiver valor na direita...
+                    noAuxiliar = noAuxiliar.getDireita();//...pega o valor da direita de noAuxiliar e reatribui a noAuxiliar para uma nova comparação
+                }
+            }
+        }
     }
 
     public T remover(T valor){
