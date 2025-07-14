96c62bfe3e0b8183952be81eb6bad2434ccf180e
Thiago Ferreira Ribeiro <thiago26ribeiro@gmail.com>
2025-07-14 15:47:56 -0300

    Interface Arborizavel - estudo de BST

---
+++ Arvores/ArvoreBinariaDeBusca/Conceito/Arborizavel.java
@@ -0,0 +1,13 @@
+package Arvores.ArvoreBinariaDeBusca.Conceito;
+
+public interface Arborizavel<T> {
+    void inserir(T valor);
+    T remover(T valor);
+    boolean existe(T dado);
+    void limpar();
+
+    NoTriplo<T> getRaiz();
+    String imprimirPreOrdem();
+    String imprimirEmOrdem();
+    String imprimirPosOrdem();
+}
