e02ae0735189e4065383baa3ae402cbd68365514
Thiago Ferreira Ribeiro <thiago26ribeiro@gmail.com>
2025-07-16 15:16:33 -0300

    Teste de funcionamento da ABP com impressão em Pré Ordem

--- Arvores/ArvoreBinariaDeBusca/Conceito/ArvoreBSTCLI.java
+++ Arvores/ArvoreBinariaDeBusca/Conceito/ArvoreBSTCLI.java
@@ -1,5 +1,20 @@
 package Arvores.ArvoreBinariaDeBusca.Conceito;
 
 public class ArvoreBSTCLI {
-    Arborizavel<Integer> arvore = new ArvoreBST<>();
+    public static void main(String args[]){
+        Arborizavel<Integer> arvore = new ArvoreBST<>();
+
+        arvore.inserir(1);
+        arvore.inserir(4);
+        arvore.inserir(8);
+        arvore.inserir(2);
+        arvore.inserir(11);
+        arvore.inserir(18);
+        arvore.inserir(7);
+        arvore.inserir(3);
+        arvore.inserir(20);
+        arvore.inserir(13);
+
+        System.out.println(arvore.imprimirPreOrdem());
+    }    
 }
