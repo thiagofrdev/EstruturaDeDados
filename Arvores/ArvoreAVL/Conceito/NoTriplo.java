676f5a542b4242edbb44d67291ef3ad4b1a79d8c
Thiago Ferreira Ribeiro <thiago26ribeiro@gmail.com>
2025-07-19 15:27:07 -0300

    Criação da classe NoTriplo para Árvore AVL

---
+++ Arvores/ArvoreAVL/Conceito/NoTriplo.java
@@ -0,0 +1,56 @@
+package Arvores.ArvoreAVL.Conceito;
+
+public class NoTriplo<T> {
+    private T dado;
+    private NoTriplo<T> genitor;
+    private NoTriplo<T> esquerda;
+    private NoTriplo<T> direita;
+    private int altura;
+
+    /**
+     * Construtor que inicia um nó com algura inicial em 0
+     */
+    public NoTriplo(){
+        altura = 0;
+    }
+
+    public T getDado() {
+        return dado;
+    }
+
+    public void setDado(T dado) {
+        this.dado = dado;
+    }
+
+    public NoTriplo<T> getGenitor() {
+        return genitor;
+    }
+
+    public void setGenitor(NoTriplo<T> genitor) {
+        this.genitor = genitor;
+    }
+
+    public NoTriplo<T> getEsquerda() {
+        return esquerda;
+    }
+
+    public void setEsquerda(NoTriplo<T> esquerda) {
+        this.esquerda = esquerda;
+    }
+
+    public NoTriplo<T> getDireita() {
+        return direita;
+    }
+
+    public void setDireita(NoTriplo<T> direita) {
+        this.direita = direita;
+    }
+
+    public int getAltura() {
+        return altura;
+    }
+
+    public void setAltura(int altura) {
+        this.altura = altura;
+    }
+}
